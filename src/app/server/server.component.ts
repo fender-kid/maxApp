ddca9397730c09115ef456d94537e64da3b1a4aa
fender-kid <jtt.sides@gmail.com>
2023-10-15 20:48:23 -0500

    Added a timeout to remove the server created message after 3 seconds.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,6 +10,7 @@ export class ServerComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Test Server';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -22,7 +23,14 @@ export class ServerComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server created! Server name: ' + this.serverName;
+    this.serverCreated = true;
+
+    setTimeout(() => {
+      this.serverCreated = false;
+    }, 3000);
   }
+
+
 
   onUpdateServer(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
